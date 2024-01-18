40e08f5cdfd6faeaa9adf2d25529703797ae311a
JGoo99 <zzgg3040@naver.com>
2024-01-18 12:13:06 +0900

    Initial commit

---
+++ src/main/java/com/goo/bikerelocationproject/BikeRelocationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.goo.bikerelocationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRelocationProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BikeRelocationProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/goo/bikerelocationproject/BikeRelocationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goo.bikerelocationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRelocationProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
